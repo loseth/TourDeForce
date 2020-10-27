8961c4884ef1386fbb9c0ad2bd1d3505c4ebc682
Tor Rafsol Løseth <rafsol@mac.com>
2020-10-27 10:20:58 +0100

    Initial commit.

---
+++ TourDeForce/TourDeForceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourDeForceApp.swift
+//  TourDeForce
+//
+//  Created by Tor1 on 2020-10-27.
+//
+
+import SwiftUI
+
+@main
+struct TourDeForceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
