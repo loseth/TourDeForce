14f28a5cbf48f79e4928283877d6f8f9d287e419
Tor Rafsol Løseth <rafsol@mac.com>
2020-11-03 23:08:03 +0100

    Add toolbar for adding real projects.

--- TourDeForce/Views/ProjectsView.swift
+++ TourDeForce/Views/ProjectsView.swift
@@ -12,6 +12,9 @@ struct ProjectsView: View {
     static let openTag: String? = "Open"
     static let closedTag: String? = "Closed"
     
+    @EnvironmentObject var dataController: DataController
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
     let showClosedProjects: Bool
     let projects: FetchRequest<Project>
     
@@ -36,6 +39,20 @@ struct ProjectsView: View {
             }
             .listStyle(InsetGroupedListStyle())
             .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
+            .toolbar {
+                if showClosedProjects == false {
+                    Button {
+                        withAnimation {
+                            let project = Project(context: managedObjectContext)
+                            project.closed = false
+                            project.creationDate = Date()
+                            dataController.save()
+                        }
+                    } label: {
+                        Label("Add Project", systemImage: "plus")
+                    }
+                }
+            }
         }
     }
 }
