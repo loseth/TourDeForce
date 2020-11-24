74615899291aa47a2c3b07385ae02d835c56cc29
Tor Rafsol Løseth <rafsol@mac.com>
2020-11-24 06:18:00 +0100

    Add extension for wrapping UIColor background colors.

---
+++ TourDeForce/Extensions/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  TourDeForce
+//
+//  Created by Tor Rafsol Løseth on 2020-11-24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
