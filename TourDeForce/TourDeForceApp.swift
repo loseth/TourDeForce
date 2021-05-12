43346f10aea55acaa85fed49a81e74aae0376183
Tor Rafsol Løseth <rafsol@mac.com>
2021-05-12 14:58:01 +0200

    feat: make instance of the unlock manager with a data controller dependency.

--- TourDeForce/TourDeForceApp.swift
+++ TourDeForce/TourDeForceApp.swift
@@ -10,10 +10,14 @@ import SwiftUI
 @main
 struct TourDeForceApp: App {
     @StateObject var dataController: DataController
+    @StateObject var unlockManager: UnlockManager
 
     init() {
         let dataController = DataController()
+        let unlockManager = UnlockManager(dataController: dataController)
+
         _dataController = StateObject(wrappedValue: dataController)
+        _unlockManager = StateObject(wrappedValue: unlockManager)
     }
 
     var body: some Scene {
@@ -21,6 +25,7 @@ struct TourDeForceApp: App {
             ContentView()
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(dataController)
+                .environmentObject(unlockManager)
                 // Automatically save when we detect that we are no longer
                 // the forground app. Use this rather than the scene phase
                 // API so we can port to macOS, where scene phase won't detect
