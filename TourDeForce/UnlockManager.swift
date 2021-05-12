e1e1024c11db05e8de7d6cdf2a54b44264cd4bf3
Tor Rafsol Løseth <rafsol@mac.com>
2021-05-12 14:44:35 +0200

    feat: add start/stop monitoring of the transaction queue.

--- TourDeForce/UnlockManager.swift
+++ TourDeForce/UnlockManager.swift
@@ -19,6 +19,27 @@ class UnlockManager: NSObject, ObservableObject, SKPaymentTransactionObserver, S
 
     @Published var requestState = RequestState.loading
 
+    private let dataController: DataController
+    private let request: SKProductsRequest
+    private let loadedProducts = [SKProduct]()
+
+    init(dataController: DataController) {
+        self.dataController = dataController
+
+        let productIDs = Set(["com.codecabal.TourDeForce.unlock"])
+        request = SKProductsRequest(productIdentifiers: productIDs)
+
+        super.init()
+
+        SKPaymentQueue.default().add(self)
+        request.delegate = self
+        request.start()
+    }
+
+    deinit {
+        SKPaymentQueue.default().remove(self)
+    }
+
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
 
     }
