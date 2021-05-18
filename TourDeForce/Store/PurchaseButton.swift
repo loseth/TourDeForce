738d2054d04abe1f0fa8031cd1c5c854fe09d410
Tor Rafsol Løseth <rafsol@mac.com>
2021-05-18 10:47:42 +0200

    feat: add buttonstyle for IAP purchase.

---
+++ TourDeForce/Store/PurchaseButton.swift
@@ -0,0 +1,19 @@
+//
+//  PurchaseButton.swift
+//  TourDeForce
+//
+//  Created by Tor Rafsol Løseth on 2021-05-18.
+//
+
+import SwiftUI
+
+struct PurchaseButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: 200, minHeight: 44)
+            .background(Color("Light Blue"))
+            .clipShape(Capsule())
+            .foregroundColor(.white)
+            .opacity(configuration.isPressed ? 0.5 : 1)
+    }
+}
