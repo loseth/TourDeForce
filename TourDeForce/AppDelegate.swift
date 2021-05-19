828ce0078280911ba47e084ead62f4a715c00860
Tor Rafsol Løseth <rafsol@mac.com>
2021-05-19 13:50:42 +0200

    feat: add delegate stubs for UIKit functionality for Quick Action.

---
+++ TourDeForce/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  TourDeForce
+//
+//  Created by Tor Rafsol Løseth on 2021-05-19.
+//
+
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+}
---
+++ TourDeForce/SceneDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SceneDelegate.swift
+//  TourDeForce
+//
+//  Created by Tor Rafsol Løseth on 2021-05-19.
+//
+
+import SwiftUI
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+
+}
